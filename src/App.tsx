3a570fc6588ebfe7904679b682e6d313a024600d
nikza <156214902+nikzaim@users.noreply.github.com>
2024-05-09 22:59:11 +0800

    add fuel price component

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,61 @@
-// import FuelPrice from "./components/FuelPrice";
-import axios from "axios";
-import { useEffect, useState } from "react";
+import FuelPrice from "./components/FuelPrice";
+// import axios from "axios";
+// import { useEffect, useState } from "react";
 
-type PriceType = {
-  date: string;
-  ron95: number;
-  ron97: number;
-  diesel: number;
-  series_type: string;
-};
+// type PriceType = {
+//   date: string;
+//   ron95: number;
+//   ron97: number;
+//   diesel: number;
+//   series_type: string;
+// };
 
 function App() {
-  const [date, setDate] = useState("");
-  const [ron95, setRon95] = useState(0);
-  const [ron97, setRon97] = useState(0);
-  const [diesel, setDiesel] = useState(0);
+  // const [date, setDate] = useState("");
+  // const [ron95, setRon95] = useState(0);
+  // const [ron97, setRon97] = useState(0);
+  // const [diesel, setDiesel] = useState(0);
 
-  useEffect(() => {
-    axios.get("https://api.data.gov.my/data-catalogue/?id=fuelprice").then((res) => {
-      const weeklyPrice = res.data;
-      console.log(
-        weeklyPrice
-          .filter((item: PriceType) => item.series_type === "level")
-          .map((item: PriceType) => {
-            return {
-              date: item.date,
-              ron95: item.ron95,
-              ron97: item.ron97,
-              diesel: item.diesel,
-            };
-          })
-      );
-      const filter = weeklyPrice
-        .filter((item: PriceType) => item.series_type === "level")
-        .map((item: PriceType) => {
-          return {
-            date: item.date,
-            ron95: item.ron95,
-            ron97: item.ron97,
-            diesel: item.diesel,
-          };
-        });
-      // console.log(filter.pop());
-      console.log(filter[filter.length - 1]);
-      const latest = filter[filter.length - 1];
-      setDate(latest.date);
-      setRon95(latest.ron95);
-      setRon97(latest.ron97);
-      setDiesel(latest.diesel);
-    });
-  });
+  // useEffect(() => {
+  //   axios.get("https://api.data.gov.my/data-catalogue/?id=fuelprice").then((res) => {
+  //     const weeklyPrice = res.data;
+  //     console.log(
+  //       weeklyPrice
+  //         .filter((item: PriceType) => item.series_type === "level")
+  //         .map((item: PriceType) => {
+  //           return {
+  //             date: item.date,
+  //             ron95: item.ron95,
+  //             ron97: item.ron97,
+  //             diesel: item.diesel,
+  //           };
+  //         })
+  //     );
+  //     const filter = weeklyPrice
+  //       .filter((item: PriceType) => item.series_type === "level")
+  //       .map((item: PriceType) => {
+  //         return {
+  //           date: item.date,
+  //           ron95: item.ron95,
+  //           ron97: item.ron97,
+  //           diesel: item.diesel,
+  //         };
+  //       });
+  //     // console.log(filter.pop());
+  //     console.log(filter[filter.length - 1]);
+  //     const latest = filter[filter.length - 1];
+  //     setDate(latest.date);
+  //     setRon95(latest.ron95);
+  //     setRon97(latest.ron97);
+  //     setDiesel(latest.diesel);
+  //   });
+  // });
 
   return (
     <>
       <center>
         <div className="px-20">{/* <FuelPrice></FuelPrice> */}</div>
-        <h1>DATE: {date}</h1>
-        <h1>RON95: {ron95}</h1>
-        <h1>RON97: {ron97}</h1>
-        <h1>DIESEL: {diesel}</h1>
+        <FuelPrice />
       </center>
     </>
   );
--- src/components/FuelPrice.tsx
+++ src/components/FuelPrice.tsx
@@ -1,35 +1,74 @@
-// import { useState } from "react";
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 type PriceType = {
   date: string;
   ron95: number;
   ron97: number;
+  diesel: number;
   series_type: string;
 };
 
-function getPrice() {
-  axios.get("https://api.data.gov.my/data-catalogue/?id=fuelprice").then((res) => {
-    const weeklyPrice = res.data;
-    console.log(weeklyPrice.filter((item: PriceType) => item.series_type === "level"));
-    weeklyPrice
-      .filter((item: PriceType) => item.series_type === "level")
-      .map((item: PriceType) => {
-        return {
-          date: item.date,
-          ron95: item.ron95,
-          ron97: item.ron97,
-        };
-      });
-  });
-}
-
 export default function FuelPrice() {
-  // const [ron95] = useState(100);
-  getPrice;
+  const [date, setDate] = useState("");
+  const [ron95, setRon95] = useState(0);
+  const [ron97, setRon97] = useState(0);
+  const [diesel, setDiesel] = useState(0);
+
+  useEffect(() => {
+    axios.get("https://api.data.gov.my/data-catalogue/?id=fuelprice").then((res) => {
+      const weeklyPrice = res.data;
+      console.log(
+        weeklyPrice
+          .filter((item: PriceType) => item.series_type === "level")
+          .map((item: PriceType) => {
+            return {
+              date: item.date,
+              ron95: item.ron95,
+              ron97: item.ron97,
+              diesel: item.diesel,
+            };
+          })
+      );
+      const filter = weeklyPrice
+        .filter((item: PriceType) => item.series_type === "level")
+        .map((item: PriceType) => {
+          return {
+            date: item.date,
+            ron95: item.ron95,
+            ron97: item.ron97,
+            diesel: item.diesel,
+          };
+        });
+      // console.log(filter.pop());
+      console.log(filter[filter.length - 1]);
+      const latest = filter[filter.length - 1];
+      setDate(latest.date);
+      setRon95(latest.ron95);
+      setRon97(latest.ron97);
+      setDiesel(latest.diesel);
+    });
+  });
   return (
     <div>
-      <h1>Test</h1>
+      <div className="flex justify-between">
+        <div className="bg-white p-4 border border-gray-300 rounded-md w-1/5">
+          <h1>DATE</h1>
+          <p>{date}</p>
+        </div>
+        <div className="bg-white p-4 border border-gray-300 rounded-md w-1/5">
+          <h1>RON95</h1>
+          <p>{ron95}</p>
+        </div>
+        <div className="bg-white p-4 border border-gray-300 rounded-md w-1/5">
+          <h1>RON97</h1>
+          <p>{ron97}</p>
+        </div>
+        <div className="bg-white p-4 border border-gray-300 rounded-md w-1/5">
+          <h1>DIESEL</h1>
+          <p>{diesel}</p>
+        </div>
+      </div>
     </div>
   );
 }
