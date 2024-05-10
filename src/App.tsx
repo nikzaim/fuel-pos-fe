66cae788b0e2c70f2d66759acdf77e2e139e9583
zulhusni2003 <zulhusnifamile@gmail.com>
2024-05-10 13:03:18 +0800

    Added AreaGraph & filtered the data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, LineChart, Line } from "recharts";
+import { XAxis, YAxis, CartesianGrid, Tooltip, Legend, AreaChart, Area } from "recharts";
 
 function App() {
   const [fuelPriceData, setFuelPriceData] = useState<any>(null);
@@ -27,65 +27,36 @@ function App() {
     }
   };
 
-  const renderBarChart = () => {
+  const AreaChartGraph = () => {
     return (
-      <BarChart width={800} height={400} data={filteredData}>
-        <CartesianGrid strokeDasharray="3 3" />
+      <AreaChart width={800} height={400} data={filteredData}>
+        <defs>
+          <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#FFB74D" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="#FFB74D" stopOpacity={0} />
+          </linearGradient>
+          <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#81C784" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="#81C784" stopOpacity={0} />
+          </linearGradient>
+        </defs>
         <XAxis dataKey="date" />
-        <YAxis tickFormatter={(value) => `RM${value}`} />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="ron95" fill="#FDDA0D" name="RON95" />
-        <Bar dataKey="ron97" fill="green" name="RON97" />
-        <Bar dataKey="diesel" fill="black" name="DIESEL" />
-      </BarChart>
-    );
-  };
-
-  const renderLineChart = () => {
-    return (
-      <LineChart width={800} height={400} data={filteredData}>
+        <YAxis domain={[0, "auto"]} />
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="date" />
-        <YAxis tickFormatter={(value) => `RM${value}`} />
         <Tooltip />
+        <Area type="monotone" dataKey="ron95" stroke="#FFB74D" fillOpacity={0.8} fill="url(#colorUv)" name="RON95" />
+        <Area type="monotone" dataKey="ron97" stroke="#81C784" fillOpacity={0.8} fill="url(#colorPv)" name="RON97" />
+        <Area type="monotone" dataKey="diesel" stroke="#333333" fillOpacity={0.8} fill="url(#colorUv)" name="DIESEL" />
         <Legend />
-        <Line type="monotone" dataKey="ron95" stroke="#FDDA0D" name="RON95" />
-        <Line type="monotone" dataKey="ron97" stroke="green" name="RON97" />
-        <Line type="monotone" dataKey="diesel" stroke="black" name="DIESEL" />
-      </LineChart>
+      </AreaChart>
     );
   };
 
-  /* 
-  const renderLineChart2 = () => {
-    return (
-      <ResponsiveContainer width="100%" height="100%">
-        <PieChart>
-          <Pie dataKey="ron95" data={filteredData} cx="25%" cy="50%" innerRadius={40} outerRadius={80} fill="#FDDA0D" label />
-          <Pie dataKey="ron97" data={filteredData} cx="50%" cy="50%" innerRadius={40} outerRadius={80} fill="green" label />
-          <Pie dataKey="diesel" data={filteredData} cx="75%" cy="50%" innerRadius={40} outerRadius={80} fill="black" label />
-          <Tooltip />
-        </PieChart>
-      </ResponsiveContainer>
-    );
-  };
-  */
-
-  const filteredData = fuelPriceData?.filter((item: any) => item.ron95 > 0 || item.ron97 > 0 || item.diesel > 0);
+  const filteredData = fuelPriceData?.filter((item: any) => item.ron95 > 1 || item.ron97 > 1 || item.diesel > 1);
 
   return (
     <div className="App">
-      <div className="data-container">
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
-          <>
-            {renderBarChart()}
-            {renderLineChart()}
-          </>
-        )}
-      </div>
+      <div className="data-container">{loading ? <p>Loading...</p> : <>{AreaChartGraph()}</>}</div>
     </div>
   );
 }
